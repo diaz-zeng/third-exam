bd8952b1501719326344b0221e7864ae18cca632
Diaz <zly981026@outlook.com>
2019-02-13 17:52:25 +0800

    update#20190213-1752

---
+++ src/main/resources/thirdExam_bill_type.sql
@@ -0,0 +1,8 @@
+INSERT INTO thirdExam.bill_type (type_name) VALUES ('支出');
+INSERT INTO thirdExam.bill_type (type_name) VALUES ('收入');
+INSERT INTO thirdExam.bill_type (type_name) VALUES ('转账');
+INSERT INTO thirdExam.bill_type (type_name) VALUES ('借出');
+INSERT INTO thirdExam.bill_type (type_name) VALUES ('借入');
+INSERT INTO thirdExam.bill_type (type_name) VALUES ('还入');
+INSERT INTO thirdExam.bill_type (type_name) VALUES ('还出');
+COMMIT;
